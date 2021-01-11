dffc9c842251072f92ebfd32a85a383c644c066e
jotheesjo <jothees.clouddreams@gmail.com>
2021-01-11 20:14:53 +0530

    version 1

--- admin/db.php
+++ admin/db.php
@@ -1,5 +1,6 @@
 <?php 
-$conn =mysqli_connect("localhost","root","","invicts");
+// $conn =mysqli_connect("localhost","root","","invicts");
+$conn =mysqli_connect("localhost","icloudcs_jothees","Jothees@123#","icloudcs_invicts");
 date_default_timezone_set('Asia/Kolkata');
 define("ADMINSESSION","admin_session_");
 define("IMGPATH","assets/images/users/");
