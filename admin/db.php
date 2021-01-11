e3c79bd4fa064a4ef5ca816a4ae2932b28ac7def
jotheesjo <jothees.clouddreams@gmail.com>
2021-01-11 13:38:45 +0530

    db

--- admin/db.php
+++ admin/db.php
@@ -3,7 +3,5 @@
 $conn =mysqli_connect("localhost","root","","invicts");
 date_default_timezone_set('Asia/Kolkata');
 define("ADMINSESSION","admin_session_");
-define("EMPLOYEESESSION","emp_session_");
-define("SUBADMINSESSION","subadmin_session_");
 define("IMGPATH","assets/images/users/");
 ?>
